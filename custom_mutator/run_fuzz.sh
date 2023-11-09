ff1bf4fcc28df673e77ed72f3403eb60d5c7ba0a
X3eRo0 <psinghania929@gmail.com>
2023-11-08 17:04:58 -0700

    asdfasf

--- custom_mutator/run_fuzz.sh
+++ custom_mutator/run_fuzz.sh
@@ -2,7 +2,7 @@
 
 AFL_DISABLE_TRIM=1 \
 AFL_CUSTOM_MUTATOR_ONLY=1 \
-AFL_CUSTOM_MUTATOR_LIBRARY=$HOME/libprotobuf-mutator_fuzzing_learning/5_libprotobuf_aflpp_custom_mutator_input/lpm_aflpp_custom_mutator_input.so \
+AFL_CUSTOM_MUTATOR_LIBRARY=./lpm_aflpp_custom_mutator_input.so \
 AFL_SKIP_CPUFREQ=1 \
-$HOME/AFLplusplus/afl-fuzz -i ./in -o ./out ./vuln
+/opt/AFLplusplus/afl-fuzz -i ./in -o ./out ./vuln
 
